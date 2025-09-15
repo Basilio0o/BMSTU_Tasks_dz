f4086433fbefb71657a5e7a52755e1fa7ebe943d
Martsul Vasiliy <VasilMartsul@bk.ru>
2025-09-15 21:15:23 +0300

    Добавил второе задание

---
+++ Tasks_1/T_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l, w;
+    cout << "Enter length and width ";
+    cin >> l >> w;
+    cout << "Area is " << l*w;
+    return 0;
+}
